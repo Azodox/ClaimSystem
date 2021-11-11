10c2abbc20499e7715700da2e765378acf1dcba9
Azodox <likemoi99@gmail.com>
2021-11-11 16:53:41 +0100

    Moved and added chooseStateInventory + modified loop

--- src/main/java/fr/azodox/inventory/permissions/EditPermissionsInventory.java
+++ src/main/java/fr/azodox/inventory/permissions/EditPermissionsInventory.java
@@ -1,22 +1,28 @@
-package fr.azodox.inventory;
+package fr.azodox.inventory.permissions;
 
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
 import fr.azodox.inventory.util.FastInv;
 import fr.azodox.util.HeadUtil;
 import fr.azodox.util.ItemBuilder;
 import fr.azodox.util.WGRegionUtil;
-import org.bukkit.ChatColor;
 
 public class EditPermissionsInventory extends FastInv {
 
-  public EditPermissionsInventory(ProtectedRegion region){
+  public EditPermissionsInventory(Player player, ProtectedRegion region){
     super(5 * 9, ChatColor.YELLOW + "§lPermissions");
 
     var flags = WGRegionUtil.getFlags(region);
-    for (int i = 0; i < flags.size(); i++) {
-          var flag = flags.get(i);
+    int i = 0;
+    for (var flag : flags.keySet()) {
           setItem(i, new ItemBuilder(HeadUtil.getHead("permission"))
-            .setName(flag).build());
+            .setName(flag).build(), e -> {
+              new ChooseStateInventory(player, this, region, flags.get(flag)).open(player);
+            });
+          i++;
     }
   }
 }
