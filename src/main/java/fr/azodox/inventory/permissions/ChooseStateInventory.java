a0d88345307ad904f9f6e90c83048326c2fcf444
Azodox <likemoi99@gmail.com>
2021-11-11 22:19:10 +0100

    Add new state for permission

--- src/main/java/fr/azodox/inventory/permissions/ChooseStateInventory.java
+++ src/main/java/fr/azodox/inventory/permissions/ChooseStateInventory.java
@@ -1,6 +1,7 @@
 package fr.azodox.inventory.permissions;
 
 import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.RegionGroup;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
@@ -24,6 +25,13 @@ public class ChooseStateInventory extends FastInv {
     setItem(1, new ItemBuilder(Material.GREEN_CONCRETE)
       .setName(ChatColor.GREEN + "§lTout le monde").build(), e -> {
         region.setFlag(flag, StateFlag.State.ALLOW);
+        new EditPermissionsInventory(player, region).open(player);
+      });
+    
+    setItem(2, new ItemBuilder(Material.WHITE_CONCRETE)
+      .setName(ChatColor.WHITE + "§lUniquement les Membres").build(), e -> {
+        region.setFlag(flag, StateFlag.State.ALLOW);
+        region.setFlag(flag.getRegionGroupFlag(), RegionGroup.NON_MEMBERS);
         new EditPermissionsInventory(player, region).open(player);
       });
   }
