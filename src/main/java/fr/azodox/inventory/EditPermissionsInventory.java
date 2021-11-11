7342720967e3f4c0fbb05469db841dc49ac04886
Azodox <likemoi99@gmail.com>
2021-11-11 15:58:08 +0100

    bold title

--- src/main/java/fr/azodox/inventory/EditPermissionsInventory.java
+++ src/main/java/fr/azodox/inventory/EditPermissionsInventory.java
@@ -1,18 +1,16 @@
 package fr.azodox.inventory;
 
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
-
-import org.bukkit.ChatColor;
-
 import fr.azodox.inventory.util.FastInv;
 import fr.azodox.util.HeadUtil;
 import fr.azodox.util.ItemBuilder;
 import fr.azodox.util.WGRegionUtil;
+import org.bukkit.ChatColor;
 
 public class EditPermissionsInventory extends FastInv {
 
   public EditPermissionsInventory(ProtectedRegion region){
-    super(5 * 9, ChatColor.YELLOW + "Permissions");
+    super(5 * 9, ChatColor.YELLOW + "§lPermissions");
 
     var flags = WGRegionUtil.getFlags(region);
     for (int i = 0; i < flags.size(); i++) {
