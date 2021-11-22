67f93b243aa58049e5f9189803b10597055b4752
Azodox <likemoi99@gmail.com>
2021-11-22 14:53:45 +0100

    Wrote javadoc

--- src/main/java/fr/azodox/ClaimSystem.java
+++ src/main/java/fr/azodox/ClaimSystem.java
@@ -3,12 +3,15 @@ package fr.azodox;
 import fr.azodox.blocks.CBlockLI;
 import fr.azodox.blocks.ICBlock;
 import fr.azodox.events.*;
+import fr.azodox.gui.CBlockGUI;
 import fr.azodox.inventory.util.FastInvManager;
 import fr.azodox.particle.Particle;
 import fr.azodox.task.BorderTask;
 import fr.azodox.task.ParticleTask;
 import fr.azodox.util.Cuboid;
 import fr.azodox.util.HeadUtil;
+import fr.azodox.util.WGRegionUtil;
+
 import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,17 +19,35 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
+/**
+ * This class is the main class of this plugin.
+ */
 public class ClaimSystem extends JavaPlugin {
 
     public static final int MAX_CLAIMS = 1;
     public static final String PLUGIN_PREFIX = ChatColor.GOLD + "§lClaimSystem §8§l➤ " + ChatColor.YELLOW + "";
     
     private final List<ICBlock> registeredCBlocks = new ArrayList<>();
-    private final Map<Long, List<Particle>> bordersParticles = new HashMap<>();
+    /**
+     * Fill up by {@link WGRegionUtil#showBorders(org.bukkit.entity.Player, com.sk89q.worldguard.protection.regions.ProtectedRegion)} 
+     * Values used by {@link BorderTask#run()}
+    */
+    private final Map<Long, List<Particle>> bordersParticles = new HashMap<>(); 
     private final Map<UUID, Cuboid> fakesParticles = new HashMap<>();
 
     private HeadUtil headUtil;
 
+    /**
+     * This is the method called when the plugin is enabled.
+     * This will 
+     * 
+     * initialize the {@link #headUtil} variable,
+     * register the {@link FastInvManager},
+     * register the tasks,
+     * register blocks,
+     * register heads {@link HeadUtil},
+     * register the events.
+     */
     @Override
     public void onEnable() {
         headUtil = new HeadUtil();
@@ -49,6 +70,11 @@ public class ClaimSystem extends JavaPlugin {
         getLogger().info("Enabled!");
     }
 
+    /**
+     * This will register all the heads needed
+     * @see HeadUtil#getHead(String) to get one of those heads everywhere.
+     * @see <a target="_blank" href="https://minecraft-heads.com/">Site used to get heads' texture id</a>
+     */
     private void registerHeads() {
         headUtil.addHead(/*+ Head*/"9a2d891c6ae9f6baa040d736ab84d48344bb6b70d7f1a280dd12cbac4d777", "plus");
         headUtil.addHead("e4d49bae95c790c3b1ff5b2f01052a714d6185481d5b1c85930b3f99d2321674", "wrench");
@@ -57,34 +83,64 @@ public class ClaimSystem extends JavaPlugin {
         headUtil.addHead("864f779a8e3ffa231143fa69b96b14ee35c16d669e19c75fd1a7da4bf306c", "back");
     }
 
+   /**
+    * This method register all blocks' class present in the package {@link fr.azodox.blocks}
+    * Then, blocks will be accessible with {@link #getClaimBlock(int)}
+    */
     private void registerBlocks() {
         registeredCBlocks.add(new CBlockLI());
     }
 
+    /**
+     * Registering tasks used to show particles
+     * @see ParticleTask
+     * @see BorderTask
+     */
     private void registerTasks() {
         new ParticleTask(this).runTaskTimer(this, 0, 20);
         new BorderTask(this).runTaskTimer(this, 0, 20);
     }
 
+    /**
+     * This method simplifies the way to register multiple {@link Listener}
+     * @param listeners : listeners that will be registered.
+     */
     private void registerEvents(Listener...listeners){
         for (Listener listener : listeners) {
             getServer().getPluginManager().registerEvents(listener, this);
         }
     }
 
+    /**
+     * Get fakes particles managed by {@link CBlockGUI#show(ICBlock, boolean)}
+     * @return {@link #fakesParticles}
+     */
     public Map<UUID, Cuboid> getFakesParticles() {
         return fakesParticles;
     }
 
+    /**
+     * {@linkplain #registerBlocks()}
+     * @return {@link #registeredCBlocks}
+     */
     public List<ICBlock> getRegisteredCBlocks() {
         return registeredCBlocks;
     }
 
+    /**
+     * Get a claim block's instance from {@link #registeredCBlocks}
+     * @param level : Specified claim block's level wanted
+     * @return claim block's instance
+     */
     @NotNull
     public ICBlock getClaimBlock(int level){
         return registeredCBlocks.stream().filter(c -> c.getLevel(false).equals(String.valueOf(level))).findFirst().get();
     }
 
+    /**
+     * {@link #bordersParticles}
+     * @return particles showed to player when asked
+     */
     public Map<Long, List<Particle>> getBordersParticles() {
         return bordersParticles;
     }
