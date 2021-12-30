ef5f6eaa549421255a237e5515dc41f57ce38a0d
Azodox_ <likemoi99@gmail.com>
2021-12-30 03:02:56 +0100

    Changed a while loop by a if condition (caused lag issues)

--- src/main/java/fr/azodox/task/BorderTask.java
+++ src/main/java/fr/azodox/task/BorderTask.java
@@ -6,28 +6,30 @@ import fr.azodox.util.BorderUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class BorderTask extends BukkitRunnable {
 
   private final Map<Long, Player> borders;
+  private final Iterator<Entry<Long, Player>> iterator;
 
   public BorderTask(ClaimSystem claimSystem) {
       this.borders = claimSystem.getBorders();
+      this.iterator = Iterators.cycle(this.borders.entrySet());
   }
 
   @Override
   public void run() {
       if(this.borders != null && !this.borders.isEmpty()){
-          var iterator = Iterators.cycle(this.borders.entrySet());
-
-          while (iterator.hasNext()){
-              var entry = iterator.next();
-              if((System.currentTimeMillis() / 1000) - (entry.getKey() / 1000) >= 10){
-                  iterator.remove();
-                  BorderUtil.resetBorder(entry.getValue());
-              }
+        if (iterator.hasNext()){
+          var entry = iterator.next();
+          if((System.currentTimeMillis() / 1000) - (entry.getKey() / 1000) >= 10){
+              iterator.remove();
+              BorderUtil.resetBorder(entry.getValue());
           }
+        }
       }
   }
 }
