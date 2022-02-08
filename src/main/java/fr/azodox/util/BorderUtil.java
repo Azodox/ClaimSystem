21ae27f2e27c46d682ce02963158ea5050c8abaf
Azodox_ <likemoi99@gmail.com>
2022-02-08 11:29:57 +0100

    New way to create a nms world border due to 1.18.

--- src/main/java/fr/azodox/util/BorderUtil.java
+++ src/main/java/fr/azodox/util/BorderUtil.java
@@ -3,8 +3,8 @@ package fr.azodox.util;
 import net.minecraft.network.protocol.game.ClientboundInitializeBorderPacket;
 import net.minecraft.world.level.border.WorldBorder;
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_17_R1.CraftWorld;
-import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_18_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public final class BorderUtil {
@@ -17,13 +17,13 @@ public final class BorderUtil {
         var nmsWorldBorder = new WorldBorder();
 
         nmsWorldBorder.world = ((CraftWorld) world).getHandle();
-        nmsWorldBorder.setCenter(worldBorder.getCenter().getX(), worldBorder.getCenter().getZ());
-        nmsWorldBorder.setSize(worldBorder.getSize());
-        nmsWorldBorder.setDamageAmount(worldBorder.getDamageAmount());
-        nmsWorldBorder.setDamageBuffer(worldBorder.getDamageBuffer());
-        nmsWorldBorder.setWarningDistance(worldBorder.getWarningDistance());
-        nmsWorldBorder.setWarningTime(worldBorder.getWarningTime());
+        nmsWorldBorder.c(worldBorder.getCenter().getX(), worldBorder.getCenter().getZ());
+        nmsWorldBorder.a(worldBorder.getSize());
+        nmsWorldBorder.c(worldBorder.getDamageAmount());
+        nmsWorldBorder.b(worldBorder.getDamageBuffer());
+        nmsWorldBorder.c(worldBorder.getWarningDistance());
+        nmsWorldBorder.b(worldBorder.getWarningTime());
 
-        ((CraftPlayer) player).getHandle().b.sendPacket(new ClientboundInitializeBorderPacket(nmsWorldBorder));
+        ((CraftPlayer) player).getHandle().b.a(new ClientboundInitializeBorderPacket(nmsWorldBorder));
     }
 }
