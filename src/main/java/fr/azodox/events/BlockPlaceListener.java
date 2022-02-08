e530851cd79afffbd80cedc4837513fe633ad856
Azodox_ <likemoi99@gmail.com>
2022-02-08 14:14:00 +0100

    Just some fun with particles :)

--- src/main/java/fr/azodox/events/BlockPlaceListener.java
+++ src/main/java/fr/azodox/events/BlockPlaceListener.java
@@ -7,8 +7,13 @@ import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import fr.azodox.ClaimSystem;
 import fr.azodox.gui.CBlockGUI;
+import fr.azodox.particle.Particle;
+import fr.azodox.particle.ParticleData;
+import fr.azodox.particle.ParticleType;
+import fr.azodox.particle.Rotator;
 import fr.azodox.util.Cuboid;
 import fr.azodox.util.WGRegionUtil;
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -17,6 +22,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 public class BlockPlaceListener implements Listener {
 
@@ -70,7 +77,7 @@ public class BlockPlaceListener implements Listener {
 
             ProtectedCuboidRegion claim = new ProtectedCuboidRegion(
                     "claim_" +
-                    player.getUniqueId().toString() + "_"
+                            player.getUniqueId() + "_"
                     + (WGRegionUtil.getRegionsNumber(player.getLocation().getWorld(), player) + 1),
                     BukkitAdapter.asBlockVector(firstPoint),
                     BukkitAdapter.asBlockVector(secondPoint)
@@ -83,9 +90,37 @@ public class BlockPlaceListener implements Listener {
                 regions.addRegion(WGRegionUtil.setFlagsForClaim(claim));
             }
 
-            center.getWorld().getBlockAt(center).setType(Material.GOLD_BLOCK);
+            Block at = center.getWorld().getBlockAt(center);
+            at.setType(Material.GOLD_BLOCK);
 
             WGRegionUtil.whenCreate(player);
+
+            new BukkitRunnable(){
+
+                private int i = 0;
+
+                @Override
+                public void run() {
+                    i+=10;
+                    for (int degree = 0; degree < 360; degree++) {
+                        double radians = Math.toRadians(degree);
+                        double x = 6 * Math.cos(radians);
+                        double z = 6 * Math.sin(radians);
+
+                        Vector vector = Rotator.rotateAroundAxisX(new Vector(x, 0, z), i);
+
+                        Location location = at.getLocation();
+                        location.add(vector);
+
+                        var particle = new Particle(player, location, ParticleType.of(player, "redstone"), null);
+                        var particleType = particle.getType();
+                        particleType.spawn(player, location, 1, ParticleData.createDustOptions(Color.AQUA, 2));
+
+                        location.subtract(vector);
+                    }
+                }
+            }.runTaskTimer(this.claimSystem, 0, 1);
+
             e.setCancelled(true);
             player.getInventory().remove(player.getInventory().getItemInMainHand());
         }
